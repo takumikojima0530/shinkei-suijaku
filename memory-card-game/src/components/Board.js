98252f285669cc56a8edac9c2ccd2b384b66403a
Takahashi Masato <ma.takahashi@gmail.com>
2024-09-22 01:58:05 +0900

    index.css
    Tailwind CSSのインポート文追加

--- memory-card-game/src/components/Board.js
+++ memory-card-game/src/components/Board.js
@@ -3,8 +3,8 @@ import Card from './Card';
 
 const Board = ({ cards, onCardClick }) => {
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {cards.map(card => (
+    <div className='grid grid-cols-4 gap-4'>
+      {cards.map((card) => (
         <Card key={card.id} card={card} onClick={onCardClick} />
       ))}
     </div>
